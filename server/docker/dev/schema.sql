24d4c3772e1587640e6925e379b2993d316efd60
kavos113 <kohei.hadano@gmail.com>
2024-09-24 09:33:16 +0900

    fix openapi.yaml

--- server/docker/dev/schema.sql
+++ server/docker/dev/schema.sql
@@ -1,7 +1,7 @@
 create table pages (
     id int(11) not null primary key auto_increment,
     parent_id int(11) not null,
-    title text not null,
+    name text not null,
     body text not null,
     path text not null,
     created_at timestamp not null default current_timestamp,
@@ -15,4 +15,4 @@ create table tags (
     foreign key (page_id) references pages(id)
 );
 
-insert into pages (parent_id, title, body, path, creator_name) values (1, 'Home', 'Welcome to the home page', '/', 'admin');
+insert into pages (parent_id, title, name, path, creator_name) values (1, 'Home', 'Welcome to the home page', '/', 'admin');
