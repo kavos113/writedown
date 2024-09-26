11b11baead3daebef1bf3f73a34e2623e946a3d7
kavos113 <kohei.hadano@gmail.com>
2024-09-26 11:48:15 +0900

    tag service

--- server/service/tag.go
+++ server/service/tag.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"database/sql"
+	"errors"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"writedown-server/openapi"
@@ -27,19 +29,56 @@ func (t tag) GetPagesTag(ctx echo.Context, tagID int) ([]openapi.PageAbstract, e
 	if err != nil {
 		return nil, echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
 	}
+
+	res := make([]openapi.PageAbstract, 0, len(pages))
+	for _, page := range pages {
+		res = append(res, openapi.PageAbstract{
+			Id:    page.ID,
+			Path:  page.Path,
+			Title: page.Name,
+		})
+	}
+
+	return res, nil
 }
 
 func (t tag) DeletePagesPageIDTag(ctx echo.Context, pageID int, tagID int) error {
-	//TODO implement me
-	panic("implement me")
+	err := t.TagsRepository.DeleteTagByTagID(tagID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
+	}
+
+	return nil
 }
 
 func (t tag) GetPagesPageIDTag(ctx echo.Context, pageID int) ([]openapi.Tag, error) {
-	//TODO implement me
-	panic("implement me")
+	tags, err := t.TagsRepository.GetTagsByPageID(pageID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, echo.NewHTTPError(http.StatusNotFound, "tag not found")
+		}
+		return nil, echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
+	}
+
+	res := make([]openapi.Tag, 0, len(tags))
+	for _, tag := range tags {
+		res = append(res, openapi.Tag{
+			Id:   tag.ID,
+			Name: tag.Name,
+		})
+	}
+
+	return res, nil
 }
 
 func (t tag) PostPagesPageIDTag(ctx echo.Context, pageID int, req openapi.PostPagesPageIDTagJSONRequestBody) error {
-	//TODO implement me
-	panic("implement me")
+	_, err := t.TagsRepository.CreateTag(repository.Tag{
+		PageID: pageID,
+		Name:   req.Name,
+	})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
+	}
+
+	return nil
 }
